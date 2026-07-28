615c4b6973d2aa21ab82ba9a62633afac901c64e
Bhuwan Saud <bhuwansaud21@gmail.com>
2026-07-28 19:19:35 +0530

    Update Web Scraper

--- web_scraper.py
+++ web_scraper.py
@@ -4,12 +4,17 @@ from bs4 import BeautifulSoup
 
 url = 'https://quotes.toscrape.com'
 response = requests.get(url)
-soup = BeautifulSoup()
 if response.status_code == 200:
     response = response.text
     soup = BeautifulSoup(response,'lxml')
     quotes = soup.find_all('div',class_='quote')
     for quote in quotes:
-        print(quote.text)
+        quote_text = quote.find('span',class_='text').text
+        author = quote.find('small',class_='author').text
+
+        print("--Quote--")
+        print(f"Quote: {quote_text}")
+        print(f"Author: {author}")
+        print()
 else:
     print("Failed to retrieve data")
