1ff482d5b4f448e942c7bf5b3d602eb808271452
Bhuwan Saud <bhuwansaud21@gmail.com>
2026-07-28 19:40:25 +0530

    Pagination

--- web_scraper.py
+++ web_scraper.py
@@ -2,19 +2,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://quotes.toscrape.com'
-response = requests.get(url)
-if response.status_code == 200:
-    response = response.text
-    soup = BeautifulSoup(response,'lxml')
-    quotes = soup.find_all('div',class_='quote')
-    for quote in quotes:
-        quote_text = quote.find('span',class_='text').text
-        author = quote.find('small',class_='author').text
+for page in range(1,11):
 
-        print("--Quote--")
-        print(f"Quote: {quote_text}")
-        print(f"Author: {author}")
-        print()
-else:
-    print("Failed to retrieve data")
+    url = f"https://quotes.toscrape.com/page/{page}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        response = response.text
+        soup = BeautifulSoup(response,'lxml')
+        quotes = soup.find_all('div',class_='quote')
+        for quote in quotes:
+            quote_text = quote.find('span',class_='text').text
+            author = quote.find('small',class_='author').text
+            more_info = quote.find('a')['href']
+
+            print("--Quote--")
+            print(f"Quote: {quote_text}")
+            print(f"Author: {author}")
+            print(f"More about {author}: {'https://quotes.toscrape.com'+more_info}")
+            print()
+    else:
+        print("Failed to retrieve data")
